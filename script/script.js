21e2c0e6a4d310079b6957a12336f8285832f731
Ian Lucan <95251062+ianlucan@users.noreply.github.com>
2022-06-30 16:19:22 -0300

    sobre mim
    finalizado botão sobre mim.

--- script/script.js
+++ script/script.js
@@ -21,3 +21,21 @@ hamburguer.addEventListener("click", function (){
     document.querySelector(".button-menu").classList.toggle("show-menu");
     document.getElementById("x").classList.toggle("fa-x");
 });
+
+// About me
+
+function iniciaModal (modalID) {
+    const modal = document.getElementById(modalID);
+    if (modal) {
+        modal.classList.add('mostrar');
+        modal.addEventListener('click', (e) => {
+            if(e.target.id == modalID || e.target.className == 'fechar'){
+                modal.classList.remove('mostrar');
+            }
+        });
+    }
+}
+
+const sobre = document.querySelector('.sobre');
+sobre.addEventListener('click', () => iniciaModal('about-me'));
+
